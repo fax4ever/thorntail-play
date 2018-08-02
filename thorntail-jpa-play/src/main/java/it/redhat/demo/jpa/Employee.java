78085fc4c1b0b0b4e0b8438d8d899fb97409fc9b
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-08-02 16:33:52 +0200

    Add JPA application

---
+++ thorntail-jpa-play/src/main/java/it/redhat/demo/jpa/Employee.java
@@ -0,0 +1,50 @@
+package it.redhat.demo.jpa;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	private Integer id;
+
+	private String name;
+
+	private LocalDate date;
+
+	public Employee() {
+	}
+
+	public Employee(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+		this.date = LocalDate.now();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+}
